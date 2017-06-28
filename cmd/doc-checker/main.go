12d5115d285c35bc50491877500240a1096b1bd4
Raphael Randschau <nicolai86@me.com>
2017-06-28 22:15:35 +0200

    chore(docs): parse everything beforehand
    make only minimal assumptions about provider folder structure - parse all docs
    beforehand and lookup afterwards

--- cmd/doc-checker/main.go
+++ cmd/doc-checker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -11,13 +12,19 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"reflect"
 	"strings"
 )
 
+type resourceDefinition struct {
+	name string
+	fnc  string
+}
+
 type provider struct {
-	datasources []string
-	resources   []string
+	datasources []resourceDefinition
+	resources   []resourceDefinition
 }
 
 func checkFileExists(p, name string, extensions []string) (string, bool) {
@@ -28,6 +35,97 @@ func checkFileExists(p, name string, extensions []string) (string, bool) {
 		}
 	}
 	return "", false
+}
+
+// documentation contains all documentation for datasources, resources, indexed by name.
+// provider prefixes have been removed, if a datasource or resource is missing either
+// the documentation is missing or the file didn't allow classification.
+type documentation struct {
+	Datasources map[string][]byte
+	Resources   map[string][]byte
+}
+
+type docType int
+
+var (
+	docTypeDatasource docType = 0
+	docTypeResource   docType = 1
+)
+
+func loadDocumentation(providerName, root string, extensions []string) (documentation, error) {
+	d := documentation{
+		Datasources: map[string][]byte{},
+		Resources:   map[string][]byte{},
+	}
+	candidate := func(path string) bool {
+		for _, ext := range extensions {
+			if strings.HasSuffix(path, ext) {
+				return true
+			}
+		}
+		return false
+	}
+	return d, filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !candidate(path) {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		docset, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		docName, docType, err := classifyDoc(providerName, docset)
+		if err != nil {
+			log.Printf("Ignoring %q due to %v", path, err)
+			return nil
+		}
+
+		if docType == docTypeDatasource {
+			d.Datasources[docName] = docset
+		} else {
+			d.Resources[docName] = docset
+		}
+
+		return nil
+	})
+}
+
+func classifyDoc(providerName string, content []byte) (string, docType, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(content))
+	var t docType
+	var n string
+	var err error
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.Contains(line, "sidebar_current") {
+			continue
+		}
+
+		if strings.Contains(line, "datasource") {
+			t = docTypeDatasource
+			i := strings.LastIndex(line, "datasource-")
+			n = line[i+len("datasource-") : len(line)-1]
+			break
+		}
+		if strings.Contains(line, "resource") {
+			t = docTypeResource
+			i := strings.LastIndex(line, "resource-")
+			n = line[i+len("resource-") : len(line)-1]
+			break
+		}
+	}
+	if n == "" {
+		err = fmt.Errorf("could not find sidebar_current")
+	} else {
+		n = providerName + "_" + strings.Replace(n, " ", "_", -1)
+	}
+	return n, t, err
 }
 
 func main() {
@@ -47,70 +145,41 @@ func main() {
 	}
 	log.Printf("%#v\n", prov)
 
-	resourceBasePath := path.Join(*providerPath, "..", "website", "docs", "r")
-	for _, r := range prov.resources {
-		resourceFile := r[len(*providerName)+1:]
-		resourcePath, ok := checkFileExists(resourceBasePath, resourceFile, []string{"md", "markdown", "html.md", "html.markdown"})
-		if !ok {
-			log.Printf("resource documentation %q is missing at %q", resourceFile, resourceBasePath)
-			continue
-		}
-
-		verifyResourceAttributes(
-			*providerPath,
-			[]string{fmt.Sprintf("resource_%s.go", r), fmt.Sprintf("resource_%s.go", r[len(*providerName)+1:])},
-			resourcePath,
-		)
+	docs, err := loadDocumentation(*providerName, path.Join(*providerPath, "..", "website"), []string{"md", "markdown", "html.md", "html.markdown"})
+	if err != nil {
+		log.Fatalf("Failed to load docs: %v", err)
+	}
+	log.Printf("Datasources:\n")
+	for k, v := range docs.Datasources {
+		log.Printf("docs of %q: %d\n", k, len(v))
+	}
+	log.Printf("Resources:\n")
+	for k, v := range docs.Resources {
+		log.Printf("docs of %q: %d\n", k, len(v))
 	}
 
-	datasourceBasePath := path.Join(*providerPath, "..", "website", "docs", "d")
-	for _, ds := range prov.datasources {
-		datasourceFile := ds
-		dataSourceNames := []string{fmt.Sprintf("data_source_%s.go", ds)}
-		if strings.HasPrefix(ds, fmt.Sprintf("%s_", *providerName)) {
-			datasourceFile = ds[len(*providerName)+1:]
-			dataSourceNames = append(dataSourceNames, fmt.Sprintf("data_source_%s.go", ds[len(*providerName)+1:]))
+	filepath.Walk(*providerPath, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, "_test.go") {
+			return nil
 		}
-		datasourcePath, ok := checkFileExists(datasourceBasePath, datasourceFile, []string{"md", "markdown", "html.md", "html.markdown"})
-		if !ok {
-			log.Printf("resource documentation %q is missing at %q", datasourceFile, datasourceBasePath)
-			continue
+		if !strings.HasSuffix(path, ".go") {
+			return nil
 		}
-
-		verifyResourceAttributes(
-			*providerPath,
-			dataSourceNames,
-			datasourcePath,
-		)
-	}
+		log.Printf("TODO parse %q\n", path)
+		verifyAttributes(path, prov, docs)
+		return nil
+	})
 }
 
-func verifyResourceAttributes(sourcePath string, candidates []string, docFile string) {
-	sourceFile := ""
-	for _, candidate := range candidates {
-		if err := fileExists(path.Join(sourcePath, candidate)); err == nil {
-			sourceFile = path.Join(sourcePath, candidate)
-			break
-		}
-	}
-	if sourceFile == "" {
-		log.Printf("Failed to find candidate for %s\n", candidates)
-		return
-	}
+func verifyAttributes(path string, prov provider, docs documentation) {
 	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, sourceFile, nil, parser.ParseComments)
+	f, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
 	if err != nil {
-		log.Printf("Failed to parse %s: %s\n", sourceFile, err)
+		log.Printf("Failed to parse %s: %s\n", path, err)
 		return
 	}
 
-	docF, err := os.Open(docFile)
-	if err != nil {
-		log.Printf("Failed to open %q\n", docFile)
-		return
-	}
-	docset, _ := ioutil.ReadAll(docF)
-
+	// TODO identify type
 	for _, decl := range f.Decls {
 		fncDecl, ok := decl.(*ast.FuncDecl)
 		if !ok {
@@ -136,10 +205,37 @@ func verifyResourceAttributes(sourcePath string, candidates []string, docFile st
 			continue
 		}
 
-		// TODO verify the import path of schema is correct
+		// 	// TODO verify the import path of schema is correct
 		if selExpr.Sel.Name != "Resource" || selExpr.X.(*ast.Ident).Name != "schema" {
 			continue
 		}
+
+		var schemaType docType
+		var schemaName string
+		found := false
+		var docset []byte
+		for _, v := range prov.datasources {
+			if v.fnc == fncDecl.Name.Name {
+				schemaType = docTypeDatasource
+				schemaName = v.name
+				found = true
+				docset = docs.Datasources[v.name]
+			}
+		}
+		for _, v := range prov.resources {
+			if v.fnc == fncDecl.Name.Name {
+				schemaType = docTypeResource
+				schemaName = v.name
+				found = true
+				docset = docs.Resources[v.name]
+			}
+		}
+		if !found {
+			log.Printf("Could not find matching datasource or resource for %v\n", fncDecl.Name.Name)
+			continue
+		}
+		_ = schemaType
+		_ = schemaName
 
 		for _, elt := range fncDecl.Body.List[0].(*ast.ReturnStmt).Results[0].(*ast.UnaryExpr).X.(*ast.CompositeLit).Elts {
 			eltt, ok := elt.(*ast.KeyValueExpr)
@@ -159,17 +255,28 @@ func verifyResourceAttributes(sourcePath string, candidates []string, docFile st
 					continue
 				}
 
-				expectedMarkup := fmt.Sprintf("* `%s`", decodeString(eltt.Key.(*ast.BasicLit).Value))
+				expectedMarkup := ""
+				name := ""
+				if basic, ok := eltt.Key.(*ast.BasicLit); ok {
+					name = basic.Value
+					expectedMarkup = fmt.Sprintf("* `%s`", decodeString(basic.Value))
+				} else {
+					// TODO support constants defined elsewhere…
+					lit := eltt.Key.(*ast.Ident).Obj.Decl.(*ast.ValueSpec).Values[0].(*ast.BasicLit)
+					name = lit.Value
+					expectedMarkup = fmt.Sprintf("* `%s`", decodeString(lit.Value))
+				}
 				if !bytes.Contains(docset, []byte(expectedMarkup)) {
-					log.Printf("Missing %q in %q\n", expectedMarkup, docFile)
+					log.Printf("Missing %q in docs of %q\n", expectedMarkup, schemaName)
 				}
+				_ = name
 
-				if elv, ok := eltt.Value.(*ast.CompositeLit); ok {
-					for _, elv := range elv.Elts {
-						elvv := elv.(*ast.KeyValueExpr)
-						log.Printf("%s: %#v\n", decodeString(eltt.Key.(*ast.BasicLit).Value), elvv.Key.(*ast.Ident).Name)
-					}
-				}
+				// if elv, ok := eltt.Value.(*ast.CompositeLit); ok {
+				// 	for _, elv := range elv.Elts {
+				// 		elvv := elv.(*ast.KeyValueExpr)
+				// 		log.Printf("%s: %#v\n", decodeString(name), elvv.Key.(*ast.Ident).Name)
+				// 	}
+				// }
 			}
 		}
 	}
@@ -216,13 +323,19 @@ func parseProviderDefinition(path string) (provider, error) {
 					elttValue := elt.(*ast.KeyValueExpr).Value.(*ast.CompositeLit)
 					for _, elttt := range elttValue.Elts {
 						eltttt := elttt.(*ast.KeyValueExpr)
-						p.resources = append(p.resources, decodeString(eltttt.Key.(*ast.BasicLit).Value))
+						p.resources = append(p.resources, resourceDefinition{
+							name: decodeString(eltttt.Key.(*ast.BasicLit).Value),
+							fnc:  eltttt.Value.(*ast.CallExpr).Fun.(*ast.Ident).Name,
+						})
 					}
 				case elttKey.Name == "DataSourcesMap":
 					elttValue := elt.(*ast.KeyValueExpr).Value.(*ast.CompositeLit)
 					for _, elttt := range elttValue.Elts {
 						eltttt := elttt.(*ast.KeyValueExpr)
-						p.datasources = append(p.datasources, decodeString(eltttt.Key.(*ast.BasicLit).Value))
+						p.datasources = append(p.datasources, resourceDefinition{
+							name: decodeString(eltttt.Key.(*ast.BasicLit).Value),
+							fnc:  eltttt.Value.(*ast.CallExpr).Fun.(*ast.Ident).Name,
+						})
 					}
 				default:
 					log.Printf("ignoring provider keys %#v\n", elttKey.Name)
